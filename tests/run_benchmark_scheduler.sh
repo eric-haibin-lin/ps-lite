0e595ad50d29a58b358dec7719eeac9743a2a5f3
huangjun.4126 <huangjun.4126@bytedance.com>
2020-10-15 00:32:41 -0700

    zmq using error

--- tests/run_benchmark_scheduler.sh
+++ tests/run_benchmark_scheduler.sh
@@ -3,4 +3,4 @@ export DMLC_NUM_WORKER=1
 export DMLC_NUM_SERVER=1 
 export DMLC_PS_ROOT_URI=$ARNOLD_WORKER_0_HOST
 export DMLC_INTERFACE=eth0
-DMLC_ROLE=scheduler ./test_benchmark
+DMLC_ROLE=scheduler DMLC_ENABLE_UCX=0 ./test_benchmark
--- tests/run_benchmark_worker.sh
+++ tests/run_benchmark_worker.sh
@@ -3,4 +3,4 @@ export DMLC_NUM_WORKER=1
 export DMLC_NUM_SERVER=1 
 export DMLC_PS_ROOT_URI=$ARNOLD_WORKER_0_HOST
 export DMLC_INTERFACE=eth0
-DMLC_ROLE=worker ./test_benchmark 1024000 1000 3
+DMLC_ROLE=worker DMLC_ENABLE_UCX=0 ./test_benchmark 1024000 1000 3
