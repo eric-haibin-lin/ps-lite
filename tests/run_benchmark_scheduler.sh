fdc525e2cc02f6f2e6cfa16ff9de81a551b7e8ac
huangjun.4126 <huangjun.4126@bytedance.com>
2020-10-16 01:17:39 -0700

    basic enable rank read from env var

--- tests/run_benchmark_scheduler.sh
+++ tests/run_benchmark_scheduler.sh
@@ -1,6 +1,8 @@
 source port_util.sh
 export DMLC_NUM_WORKER=1
 export DMLC_NUM_SERVER=1 
-export DMLC_PS_ROOT_URI=$ARNOLD_WORKER_0_HOST
+export DMLC_PS_ROOT_URI=10.188.136.21
 export DMLC_INTERFACE=eth0
-DMLC_ROLE=scheduler DMLC_ENABLE_UCX=0 ./test_benchmark
+DMLC_ROLE=scheduler DMLC_ENABLE_UCX=0 PS_VERBOSE=2 ./test_benchmark
+
+#$ARNOLD_WORKER_0_HOST
--- tests/run_benchmark_server.sh
+++ tests/run_benchmark_server.sh
@@ -1,6 +1,6 @@
 source port_util.sh
 export DMLC_NUM_WORKER=1
 export DMLC_NUM_SERVER=1 
-export DMLC_PS_ROOT_URI=$ARNOLD_WORKER_0_HOST
+export DMLC_PS_ROOT_URI=10.188.136.21
 export DMLC_INTERFACE=eth0
-DMLC_ROLE=server DMLC_ENABLE_UCX=0 ./test_benchmark
+DMLC_ROLE=server DMLC_ENABLE_UCX=0 RANK=0 PS_VERBOSE=2 ./test_benchmark
