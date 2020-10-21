f2d335722fcd86fd3143927adc492c76b9430b23
huangjun.4126 <huangjun.4126@bytedance.com>
2020-10-21 06:16:25 -0700

    add test bash

--- tests/multi_server_worker_test.sh
+++ tests/multi_server_worker_test.sh
@@ -1,7 +1,7 @@
-bash ./run_benchmark_server.sh 3 3 0 &
-bash ./run_benchmark_server.sh 3 3 1 &
-bash ./run_benchmark_server.sh 3 3 2 &
+bash ./run_benchmark_server_provide_rank.sh 3 3 0 &
+bash ./run_benchmark_server_provide_rank.sh 3 3 1 &
+bash ./run_benchmark_server_provide_rank.sh 3 3 2 &
 
-bash ./run_benchmark_worker.sh 3 3 0 &
-bash ./run_benchmark_worker.sh 3 3 1 &
-bash ./run_benchmark_worker.sh 3 3 2 &
+bash ./run_benchmark_worker_provide_rank.sh 3 3 0 &
+bash ./run_benchmark_worker_provide_rank.sh 3 3 1 &
+bash ./run_benchmark_worker_provide_rank.sh 3 3 2 &
